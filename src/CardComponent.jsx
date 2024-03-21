42bdab64cdaa4191c77b4259b9a24d22cb2e1761
Potti1234 <lukas.pottner@gmail.com>
2024-03-21 13:47:16 +0100

    Added Icons in the cards

--- src/CardComponent.jsx
+++ src/CardComponent.jsx
@@ -13,27 +13,27 @@ export function CardComponent({cardData, checkMoneyIsValid, handleBuyClick}) {
                 <input type="text" value={cardData.name} readOnly />
             </div>
             <div>
-                <img src={cardData.icon} alt="Card Icon" />
+                <img src={require("./icon/propability.jpeg")} alt="Winning Probility" />
                 <input type="text" value={cardData.probability} readOnly />
             </div>
             <div>
-                <img src={cardData.icon} alt="Card Icon" />
+                <img src={require("./icon/ArrowUp.jpeg")} alt="Profit Percent" />
                 <input type="text" value={cardData.winPercentage} readOnly />
             </div>
             <div>
-                <img src={cardData.icon} alt="Card Icon" />
+                <img src={require("./icon/ArrowDown.jpeg")} alt="Loss Percent" />
                 <input type="text" value={cardData.lossPercentage} readOnly />
             </div>
             <div>
-                <img src={cardData.icon} alt="Card Icon" />
+                <img src={require("./icon/tree.jpeg")} alt="EcologyRating" />
                 <input type="text" value={cardData.ecologyRating} readOnly />
             </div>
             <div>
-                <img src={cardData.icon} alt="Card Icon" />
+                <img src={require("./icon/social.jpeg")} alt="Social Rating" />
                 <input type="text" value={cardData.socialRating} readOnly />
             </div>
             <div>
-                <img src={cardData.icon} alt="Card Icon" />
+                <img src={require("./icon/governance.jpeg")} alt="Governance Rating" />
                 <input type="text" value={cardData.governanceRating} readOnly />
             </div>
             <div>
