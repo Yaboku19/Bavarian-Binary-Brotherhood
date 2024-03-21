79a35e5f9ad067a075e86b48ab4a1b9e97962265
Potti1234 <lukas.pottner@gmail.com>
2024-03-22 00:11:44 +0100

    Add Tooltips for the icons of cards

--- src/CardComponent.jsx
+++ src/CardComponent.jsx
@@ -13,27 +13,27 @@ export function CardComponent({cardData, checkMoneyIsValid, handleBuyClick}) {
                 <input type="text" value={cardData.name} readOnly />
             </div>
             <div>
-                <img src={require("./icon/propability.jpeg")} alt="Winning Probility" />
+                <img src={require("./icon/propability.jpeg")} alt="Winning Probility" title="Winning probability" />
                 <input type="text" value={cardData.probability} readOnly />
             </div>
             <div>
-                <img src={require("./icon/ArrowUp.jpeg")} alt="Profit Percent" />
+                <img src={require("./icon/ArrowUp.jpeg")} alt="Profit Percent" title="Profit Percentage" />
                 <input type="text" value={cardData.winPercentage} readOnly />
             </div>
             <div>
-                <img src={require("./icon/ArrowDown.jpeg")} alt="Loss Percent" />
+                <img src={require("./icon/ArrowDown.jpeg")} alt="Loss Percent" title="Loss Percentage" />
                 <input type="text" value={cardData.lossPercentage} readOnly />
             </div>
             <div>
-                <img src={require("./icon/tree.jpeg")} alt="EcologyRating" />
+                <img src={require("./icon/tree.jpeg")} alt="EcologyRating" title="Ecology Rating" />
                 <input type="text" value={cardData.ecologyRating} readOnly />
             </div>
             <div>
-                <img src={require("./icon/social.jpeg")} alt="Social Rating" />
+                <img src={require("./icon/social.jpeg")} alt="Social Rating" title="Social Rating" />
                 <input type="text" value={cardData.socialRating} readOnly />
             </div>
             <div>
-                <img src={require("./icon/governance.jpeg")} alt="Governance Rating" />
+                <img src={require("./icon/governance.jpeg")} alt="Governance Rating" title="Governance Rating" />
                 <input type="text" value={cardData.governanceRating} readOnly />
             </div>
             <div>
